00c98d7f46db9b2fdd07f674dc6359568be35660
JaeHyeok Kim <wogur310@kookmin.ac.kr>
2025-04-24 01:12:43 +0900

    [level 0] Title: 배열 비교하기, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,18 @@
+def solution(arr1,arr2):
+    
+    sum_arr1 = 0
+    sum_arr2 = 0
+    
+    if len(arr1) > len(arr2):
+        return 1
+    elif len(arr1) == len(arr2):
+        if sum(arr1) > sum(arr2):
+            return 1
+        elif sum(arr1) == sum(arr2):
+            return 0
+        else:
+            return -1
+        
+    else:
+        return -1
+    
