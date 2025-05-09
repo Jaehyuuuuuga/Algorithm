2226d9bff8ea80f9fd911af9fd989bc91922a2bd
JaeHyeok Kim <wogur310@kookmin.ac.kr>
2025-05-09 10:47:29 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.py
@@ -1,13 +1,2 @@
 def solution(num_list, n):
-    first_ans = []
-    sec_ans = []
-    
-    cnt = 0
-    
-    for i in range(len(num_list)):
-        if cnt < n:
-            cnt +=1
-            first_ans.append(num_list[i])
-        else:
-            sec_ans.append(num_list[i])
-    return sec_ans+first_ans
+    return num_list[n:]+num_list[:n]
