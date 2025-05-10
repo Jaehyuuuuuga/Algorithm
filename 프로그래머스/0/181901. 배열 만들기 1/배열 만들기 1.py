200f50fbbfe2c367ed7176912aeaf5feae98d80b
JaeHyeok Kim <wogur310@kookmin.ac.kr>
2025-05-10 21:01:46 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.14 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,11 +1,13 @@
 def solution(n, k):
     
-    result = []
+#     result = []
     
-    for i in range(1,n+1):
+#     for i in range(1,n+1):
         
-        if i % k == 0:
-            result.append(i)
-    return result
+#         if i % k == 0:
+#             result.append(i)
+#     return result
+
+    return [i for i in range(k,n+1,k)]
         
         
