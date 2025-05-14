8605c68ec87e03e53825eb53d281300d1d3c6d17
JaeHyeok Kim <wogur310@kookmin.ac.kr>
2025-05-14 17:11:53 +0900

    [level 0] Title: 수 조작하기 1, Time: 20.10 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,14 +1,21 @@
-def solution(n, control):
+# def solution(n, control):
+    
+#     for i in control:
+        
+#         if i == "w":
+#             n += 1
+#         elif i == "s":
+#             n -= 1
+#         elif i == "d":
+#             n +=10
+#         elif i == "a":
+#             n -=10
+#     return n
+    
+def solution(n,control):
+    dic = {'w':1,'s':-1,'d':10,'a':-10}
     
     for i in control:
-        
-        if i == "w":
-            n += 1
-        elif i == "s":
-            n -= 1
-        elif i == "d":
-            n +=10
-        elif i == "a":
-            n -=10
+        n += dic[i]
     return n
     
