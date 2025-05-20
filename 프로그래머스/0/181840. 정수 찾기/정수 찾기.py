b7f4fb0255fc6b8cbae20dfad4ad4bd480fd8cd4
JaeHyeok Kim <wogur310@kookmin.ac.kr>
2025-05-21 00:22:43 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.py
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.py
@@ -1,11 +1,11 @@
 def solution(num_list, n):
-    answer = 0
+#     answer = 0
     
-    for i in num_list:
-        if i == n:
-            return 1        
+#     for i in num_list:
+#         if i == n:
+#             return 1        
         
-    return 0
+    return 1 if n in num_list else 0
             
         
     
